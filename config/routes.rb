1b0ec11cd233803103a0ff3c5b5d03a57645f6a0
evanmasterson <evanm95@hotmail.com>
2018-03-18 17:47:00 +0000

    create additional admin user to seperate access

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'users'
+  devise_for :admins, path: 'admins'
   resources :patients do
     resources :doctors
   end
