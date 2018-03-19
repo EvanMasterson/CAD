e6a357bf1a460eb5b8488b49e60b654f22ef2aea
evanmasterson <evanm95@hotmail.com>
2018-03-19 14:52:14 +0000

    change generic devise vies to user views

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'users'
   devise_for :admins, path: 'admins'
-  resources :patients do
-    resources :doctors
-  end
-  resources :doctors do
-    resources :patients
-  end
+  # resources :patients do
+  #   resources :doctors
+  # end
+  # resources :doctors do
+  #   resources :patients
+  # end
   
   root :to =>'pages#home'
   
   get '/appointment', to: 'pages#appointment'
+  get '/patients', to: 'patients#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
