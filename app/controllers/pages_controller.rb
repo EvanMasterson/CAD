6d9d376229047b6eddeefdb239a0946e9f66e6bf
evanmasterson <evanm95@hotmail.com>
2018-03-20 13:23:57 +0000

    redirect appointment to patient controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
+    before_action :authenticate_user!, only: :appointment
     def home
     end
     
     def appointment
+        @patient = redirect_to new_patient_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   
   root :to =>'pages#home'
   
-  get '/appointment', to: 'patients#new'
+  get '/appointment', to: 'pages#appointment'
   get '/patients', to: 'patients#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
