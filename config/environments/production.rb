7eddb1991f751af64317955352fc61e44d5a15d4
evanmasterson <evanm95@hotmail.com>
2018-04-02 16:51:28 +0000

    test sendgrid email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,12 +62,23 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "healthApp_#{Rails.env}"
   
   # heroku config
-  config.action_mailer.default_url_options = { :host => 'whispering-scrubland-72575.herokuapp.com:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'whispering-scrubland-72575.herokuapp.com' }
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              =>  'smtp.sendgrid.net',
+    :port                 =>  '25',
+    :authentication       =>  :plain,
+    :user_name            =>  ENV['SENDGRID_USERNAME'],
+    :password             =>  ENV['SENDGRID_PASSWORD'],
+    :domain               =>  ENV['SENDGRID_DOMAIN'],
+    :enable_starttls_auto =>  true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
